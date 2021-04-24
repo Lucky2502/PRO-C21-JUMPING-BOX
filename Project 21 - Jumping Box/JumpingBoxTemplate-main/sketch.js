14900de70a0fbd4c11eee9898c40ba3d0c4f0454
Lucky Dacha <76813306+Lucky2502@users.noreply.github.com>
2021-04-24 14:19:01 +0530

    Add files via upload
    Added Music

--- Project 21 - Jumping Box/JumpingBoxTemplate-main/sketch.js
+++ Project 21 - Jumping Box/JumpingBoxTemplate-main/sketch.js
@@ -11,7 +11,7 @@ function preload(){
 function setup(){
     canvas = createCanvas(800,600);
 
-    music.play();
+    
 
     block1 = createSprite(0,580,360,30);
     block1.shapeColor = rgb(0,0,255);
@@ -42,21 +42,19 @@ function draw() {
         music.play();
     }
 
-    if(block2.isTouching(ball)){
+    if(block2.isTouching(ball) && ball.bounceOff(block2)){
         ball.shapeColor = rgb(255,128,0);
         ball.velocityX = 0;
         ball.velocityY = 0;
         music.stop();
     }
 
-    if(block3.isTouching(ball)){
+    if(block3.isTouching(ball) && ball.bounceOff(block3)){
         ball.shapeColor = rgb(153,0,76);
-        music.play();
     }
 
     if(block4.isTouching(ball) && ball.bounceOff(block4)){
         ball.shapeColor = rgb(0,100,0);
-        music.play();
     }
 
     drawSprites();
